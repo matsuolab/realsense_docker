86d4646dc2bf0ac79d39df23e1537cc94c9b5ec0
Sean N <seann999@gmail.com>
2021-08-04 23:30:59 +0900

    Fix launch realsense

--- launch_realsense.sh
+++ launch_realsense.sh
@@ -8,21 +8,25 @@ if [ "$(uname -m)" == "aarch64" ]; then
     TAG_NAME=jetson
 fi
 
-ROS_MASTER_URI="http://`hostname -I | cut -d' ' -f1`:11311"
+#ROS_MASTER_URI="http://`hostname -I | cut -d' ' -f1`:11311"
+HSRB_HOSTNAME=hsrb.local
+HSRB_IP=`avahi-resolve -4 --name ${HSRB_HOSTNAME} | cut -f 2`
+ROS_MASTER_URI="http://${HSRB_IP}:11311"
 ROS_IP=`hostname -I | cut -d' ' -f1`
-LAUNCH=rs_camera.launch
+#LAUNCH=rs_camera.launch
+LAUNCH="rs_camera.launch camera:=hand_camera align_depth:=true respawn:=true"
 
-if [ ! $# -eq 0 ]; then
-    IP_CHECK=$(echo $1 | egrep "^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
-    if [ "${IP_CHECK}" ]; then
-        ROS_MASTER_URI="http://$1:11311"
-        if [ $# -ge 2 ]; then
-            LAUNCH=${@:2}
-        fi
-    else
-        LAUNCH=$@
-    fi
-fi
+#if [ ! $# -eq 0 ]; then
+#    IP_CHECK=$(echo $1 | egrep "^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+#    if [ "${IP_CHECK}" ]; then
+#        ROS_MASTER_URI="http://$1:11311"
+#        if [ $# -ge 2 ]; then
+#            LAUNCH=${@:2}
+#        fi
+#    else
+#        LAUNCH=$@
+#    fi
+#fi
 
 echo "IMAGE_NAME=${IMAGE_NAME}:${TAG_NAME}"
 echo "CONTAINER_NAME=${CONTAINER_NAME}"
@@ -30,7 +34,7 @@ echo "ROS_MASTER_URI=${ROS_MASTER_URI}"
 echo "ROS_IP=${ROS_IP}"
 echo "LAUNCH=${LAUNCH}"
 
-docker run -it --rm \
+docker run -it \
     --privileged \
     --volume="/dev:/dev" \
     --env ROS_MASTER_URI=${ROS_MASTER_URI} \
